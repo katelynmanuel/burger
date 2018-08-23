47cad90bfe3b2c91c54be71559fe811a54a8f30b
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-08-22 18:33:07 -0600

    Adding dependencies and initial connections.

---
+++ config/orm.js
@@ -0,0 +1,5 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//Creating ORM export.
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Patty", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beef Patty with Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken and Bacon", false);
