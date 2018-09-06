d7afafdb07b46a7b35b59d8993447097339f86a7
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-09-05 18:46:38 -0600

    Database connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  port: process.env.PORT,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-});
+// var connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   user: process.env.USERNAME,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Lem0nworld19",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
